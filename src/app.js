8de6fd41917aaa6146a493f690dfcd848c0ba048
Brylimo <icj0103@gmail.com>
2021-12-28 22:29:16 +0900

    Add globalRouter

--- src/app.js
+++ src/app.js
@@ -1,3 +1,14 @@
 import express from "express";
+import logger from "morgan";
+import globalRouter from "./routers/globalRouter";
 
-console.log("Hello world");
+const PORT = 8000;
+
+const app = express();
+
+app.use(logger("dev"));
+app.use("/", globalRouter);
+
+const handleListening = () => console.log(`✅ Server listening on port http://localhost:${PORT}`);
+
+app.listen(PORT, handleListening);

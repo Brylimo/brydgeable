5800b63b97709f9427108ba16ada79eb9fbd05e2
Brylimo <icj0103@gmail.com>
2021-12-29 15:50:05 +0900

    Add base.pug

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -1,1 +1,2 @@
 export const handleUser = (req, res) => res.send("user");
+export const handleHome = (req, res) => res.render("home", {title : "home"});
--- src/routers/globalRouter.js
+++ src/routers/globalRouter.js
@@ -1,7 +1,7 @@
 import express from "express";
+import { handleHome } from "../controllers/userController";
 
 const globalRouter = express.Router();
-const handleHome = (req, res) => res.render("home");
 
 globalRouter.get("/", handleHome);
 

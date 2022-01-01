3fcbc82fa389f2ebc6e52d47795167fda983f024
Brylimo <icj0103@gmail.com>
2022-01-01 22:29:07 +0900

    Add session

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from "express";
 import logger from "morgan";
+import session from "express-session";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import itemRouter from "./routers/itemRouter";
@@ -10,6 +11,11 @@ app.set("view engine", "pug");
 app.set("views", process.cwd() + "/src/views");
 app.use(logger("dev"));
 app.use(express.urlencoded({extended:true}));
+app.use(session({
+	secret: 'keyboard cat',
+	resave: true,
+	saveUninitialized: true
+}))
 app.use("/", globalRouter);
 app.use("/user", userRouter);
 app.use("/item", itemRouter);

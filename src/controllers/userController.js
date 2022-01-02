aa9d9e9991e5cdea0ee3e8c98a52a32928ac6e8e
Brylimo <icj0103@gmail.com>
2022-01-02 16:06:59 +0900

    Update user join

--- src/controllers/userController.js
+++ src/controllers/userController.js
@@ -5,14 +5,20 @@ export const getJoin = (req, res) => res.render("join", {title: "join"});
 export const postJoin = async (req, res) => {
 	try {
 		const { name, email, username, password, password2 } = req.body;
+		const userExist = await User.exists({ $or: [{username}, {password}] });
+		if (userExist) {
+			return res.status(400).render("join", {title: "join", errorMessage: "username/password already have taken"});
+		}
+		if (password !== password2) {
+			return res.status(400).render("join", {title: "join", errorMessage: "password and password2 are different"});
+		}
 		await User.create({
 			name,
 			email,
 			username,
-			password,
-			password2
+			password
 		});
-		res.redirect("/login");
+		return res.redirect("/login");
 	} catch(error) {
 		console.log(error);
 	}
@@ -20,11 +26,5 @@ export const postJoin = async (req, res) => {
 export const getLogin = (req, res) => res.render("login", {title : "login"});
 export const postLogin = async (req, res) => {
 	const { username, password } = req.body;
-	let errorMessage = "";
-	const exist = await User.exists({ $and: [{username}, {password}] });
-	if (!exist) {
-		errorMessage = "username/password unmatch";
-		return res.status(400).render("login", {title : "login", errorMessage });
-	}
-	return res.render("login", {title : "login", errorMessage });
+	return res.render("login", {title : "login"});
 }
--- src/models/User.js
+++ src/models/User.js
@@ -1,11 +1,15 @@
 import mongoose from "mongoose";
+import bcrypt from "bcrypt";
 
 const userSchema = new mongoose.Schema({
 	name: {type: String, required: true},
 	email: {type: String, required: true},
 	username: {type: String, required: true, unique: true},
-	password: {type: String, required: true, unique: true},
-	password2: {type: String, required: true, unique: true},
+	password: {type: String, required: true, unique: true}
+});
+
+userSchema.pre('save', async function() {
+	this.password = await bcrypt.hash(this.password, 10);
 });
 
 const User = mongoose.model("User", userSchema);

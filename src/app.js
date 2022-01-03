e6c97fca64723c347ace8f6d0b973676153f2f4f
Brylimo <icj0103@gmail.com>
2022-01-03 22:21:07 +0900

    Add connect-mongo

--- src/app.js
+++ src/app.js
@@ -1,6 +1,7 @@
 import express from "express";
 import logger from "morgan";
 import session from "express-session";
+import MongoStore from "connect-mongo";
 import globalRouter from "./routers/globalRouter";
 import userRouter from "./routers/userRouter";
 import itemRouter from "./routers/itemRouter";
@@ -15,7 +16,8 @@ app.use(express.urlencoded({extended:true}));
 app.use(session({
 	secret: 'keyboard cat',
 	resave: true,
-	saveUninitialized: true
+	saveUninitialized: false,
+	store: MongoStore.create({ mongoUrl: 'mongodb://127.0.0.1:27017/brydgeable' })
 }));
 app.use(localsMiddleware);
 app.use("/", globalRouter);

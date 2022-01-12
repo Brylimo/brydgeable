4127c799890ac1daa0405efd0928a2b5606661f4
Brylimo <icj0103@gmail.com>
2022-01-12 14:12:14 +0900

    Add scss

--- src/controllers/globalController.js
+++ src/controllers/globalController.js
@@ -2,7 +2,7 @@ import Item from "../models/Item";
 
 export const handleHome = async (req, res) => {
 	try {
-		const items = await Item.find({});
+		const items = await Item.find({}).sort({createdAt: "desc"}).populate("owner");
 		return res.render("home", {title : "home", items});
 	} catch(error) {
 		console.log(error);

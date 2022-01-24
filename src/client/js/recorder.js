a2ca86d96e7562c1552d66167822602ba6ccca88
Brylimo <icj0103@gmail.com>
2022-01-24 17:39:11 +0900

    Add recorder

---
+++ src/client/js/recorder.js
@@ -0,0 +1,40 @@
+const startBtn = document.getElementById("startBtn");
+const video = document.getElementById("preview");
+
+let stream;
+let recorder;
+
+const handleDownload = () => {}
+
+const handleStop = () => {
+	startBtn.innerText = "Download Recording";
+	startBtn.removeEventListener("click", handleStop);
+	startBtn.addEventListener("click", handleDownload);
+
+	recorder.stop();
+}
+
+const handleStart = () => {
+	startBtn.innerText = "Stop Recording";
+	startBtn.removeEventListener("click", handleStart);
+	startBtn.addEventListener("click", handleStop);
+	recorder = new MediaRecorder(stream);
+	recorder.ondataavailable = (event) => {
+		const videoFile = URL.createObjectURL(event.data);
+		console.log(videoFile);
+	};
+	recorder.start();
+}
+
+const init = async() => {
+	stream = await navigator.mediaDevices.getUserMedia({
+		audio:false,
+		video:{width:200, height:100},
+	});
+	video.srcObject = stream;
+	video.play();
+}
+
+init();
+
+startBtn.addEventListener("click", handleStart);

b57d6c93646122a9d0e6426ef6cad377b8f5812e
Brylimo <icj0103@gmail.com>
2022-01-24 17:59:58 +0900

    complete recorder.js

--- src/client/js/recorder.js
+++ src/client/js/recorder.js
@@ -3,8 +3,15 @@ const video = document.getElementById("preview");
 
 let stream;
 let recorder;
+let videoFile;
 
-const handleDownload = () => {}
+const handleDownload = () => {
+	const a = document.createElement("a");
+	a.href = videoFile;
+	a.download = "MyRecording.webm";
+	document.body.appendChild(a);
+	a.click(); 
+}
 
 const handleStop = () => {
 	startBtn.innerText = "Download Recording";
@@ -18,10 +25,13 @@ const handleStart = () => {
 	startBtn.innerText = "Stop Recording";
 	startBtn.removeEventListener("click", handleStart);
 	startBtn.addEventListener("click", handleStop);
-	recorder = new MediaRecorder(stream);
+	recorder = new MediaRecorder(stream, { mimeType: "video/webm" });
 	recorder.ondataavailable = (event) => {
-		const videoFile = URL.createObjectURL(event.data);
-		console.log(videoFile);
+		videoFile = URL.createObjectURL(event.data);
+		video.srcObject = null;
+		video.src = videoFile;
+		video.loop = true;
+		video.play();
 	};
 	recorder.start();
 }
